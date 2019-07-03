c0b2d14817b9f5119a47e80f05c23a32bd53cddc
glala981 <eogkr2017@daum.net>
2019-07-03 15:02:20 +0900

    불필요한 디자인 파일은 삭제하고, kingdom 이미지를 추가함

--- app/src/main/java/com/swu/swu_semi_project/MainActivity.java
+++ app/src/main/java/com/swu/swu_semi_project/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+// 소스도 수정함 
 
         // 테스트 코드 1
 
